1e0c5123bd17288c9590b1d6f764a8d02d6eaa68
eliaschavezfuentes <132020494+eliaschavezfuentes@users.noreply.github.com>
2023-08-03 17:00:34 -0600

    Add files via upload

---
+++ actividad-práctica-20230803T221620Z-001/actividad-práctica/activity1.js
@@ -0,0 +1,14 @@
+score = 0
+
+function updateScore(){
+sore = scre+1;
+document.getElementById("score").innerHTML = "Puntuacion: " +score;
+}
+
+function saveScore(){
+localStorage.setItem("score", score);
+}
+
+function updateScore(){
+    window.localitation = "activity_2.html";
+    }
